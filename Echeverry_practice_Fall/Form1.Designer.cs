fdfe35459932dcb04ec59ed8730d1f3cb8687026
Student <Student@A414-23>
2026-09-09 11:33:49 -0400

    Add project files.

---
+++ Echeverry_practice_Fall/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Echeverry_practice_Fall
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtTextInput = new TextBox();
+            label3 = new Label();
+            txtNumericInput = new TextBox();
+            lstOut = new ListBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            label1.ForeColor = Color.Blue;
+            label1.Location = new Point(125, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(237, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Sample Transaction Form";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11F);
+            label2.Location = new Point(23, 79);
+            label2.Name = "label2";
+            label2.Size = new Size(118, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Text Based Input";
+            // 
+            // txtTextInput
+            // 
+            txtTextInput.Location = new Point(222, 79);
+            txtTextInput.Name = "txtTextInput";
+            txtTextInput.Size = new Size(100, 23);
+            txtTextInput.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11F);
+            label3.Location = new Point(23, 138);
+            label3.Name = "label3";
+            label3.Size = new Size(147, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Numeric Based Input";
+            // 
+            // txtNumericInput
+            // 
+            txtNumericInput.Location = new Point(222, 139);
+            txtNumericInput.Name = "txtNumericInput";
+            txtNumericInput.Size = new Size(100, 23);
+            txtNumericInput.TabIndex = 4;
+            // 
+            // lstOut
+            // 
+            lstOut.FormattingEnabled = true;
+            lstOut.Location = new Point(23, 188);
+            lstOut.Name = "lstOut";
+            lstOut.Size = new Size(411, 109);
+            lstOut.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(23, 336);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 6;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(195, 336);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 7;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(359, 336);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 8;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(586, 450);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(lstOut);
+            Controls.Add(txtNumericInput);
+            Controls.Add(label3);
+            Controls.Add(txtTextInput);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Echeverry Sample Program";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtTextInput;
+        private Label label3;
+        private TextBox txtNumericInput;
+        private ListBox lstOut;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
---
+++ Echeverry_practice_Fall/Form1.cs
@@ -0,0 +1,15 @@
+namespace Echeverry_practice_Fall
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
